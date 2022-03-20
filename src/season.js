edf8c632ffb4fc8abca4311123f99d46eb6258df
Faison Zutavern <faisonz@protonmail.com>
2022-03-19 22:29:16 -0500

    Cleanup the season file and add jsdocs

--- src/season.js
+++ src/season.js
@@ -3,22 +3,21 @@ const DAY_IN_MS = 24 * HOUR_IN_MS;
 const WEEK_IN_MS = 7 * DAY_IN_MS;
 const SEASON_IN_MS = 13 * WEEK_IN_MS;
 const YEAR_IN_MS = 4 * SEASON_IN_MS;
-const JUBILEE_IN_MS = 7 * YEAR_IN_MS;
 
 /**
  * @typedef {object} Season
- * @property {Date} spring - .
- * @property {Date} summer - .
- * @property {Date} fall - .
- * @property {Date} winter - .
+ * @property {Date} spring - The start date of spring.
+ * @property {Date} summer - The start date of summer.
+ * @property {Date} fall   - The start date of fall.
+ * @property {Date} winter - The start date of winter.
  */
 
 /**
- * .
+ * Calculates the start of spring from the provided date for the number of years provided.
  *
- * @param {number} num - .
- * @param {Date} startingSpring - .
- * @returns {Date[]} .
+ * @param {number} num          - The number of years to calculate.
+ * @param {Date} startingSpring - The start of spring to calculate from.
+ * @returns {Date[]} An array of spring start dates.
  */
 const getManySprings = (num, startingSpring) => {
   const springs = [startingSpring];
@@ -31,10 +30,10 @@ const getManySprings = (num, startingSpring) => {
 }
 
 /**
- * .
+ * Takes a starting spring date and calculates the rest of the seasons; start dates.
  *
- * @param {Date} startDate - .
- * @returns {Season} .
+ * @param {Date} startDate - The start of spring to calculate from.
+ * @returns {Season} The start of all seasons, starting with the provided start date.
  */
 const getSeasonFromSpring = (startDate) => {
   const season = {};
@@ -48,21 +47,13 @@ const getSeasonFromSpring = (startDate) => {
 };
 
 /**
- * .
+ * Calculate the season start dates for a number of years starting from a start of spring date.
  *
- * @param {Date[]} springDates 
- * @returns {Season[]} .
- */
-const getSeasonsForSprings = (springDates) => springDates.map(getSeasonFromSpring);
-
-/**
- * .
- *
- * @param {number} num - .
- * @param {Date}   startingSpring - .
- * @returns {Season[]} .
+ * @param {number} num            - The number of years to calculate.
+ * @param {Date}   startingSpring - The start of spring to calculate from.
+ * @returns {Season[]} An array of seasonal start dates.
  */
 export const getSeasonsFromStartingSpring = (num, startingSpring) => {
   const springs = getManySprings(num, startingSpring)
-  return getSeasonsForSprings(springs);
+  return springs.map(getSeasonFromSpring);
 }
